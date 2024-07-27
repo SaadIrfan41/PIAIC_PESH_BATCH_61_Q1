c943c091182b0f850981f7dd06e299d790fd3fd1
saix511 <saadirfan41@gmail.com>
2024-07-27 19:17:09 +0500

    first class work

---
+++ step02_Basic DataTypes & Variables/main.py
@@ -0,0 +1,20 @@
+name: str = "Pakistan"
+# variable = name
+# Data Type = str (string)
+# variable's value = "Pakistan" (string)
+print(name)  # print's variable value
+print(type(name))  # print's variable type
+print(id(name))  # variable's physcial address in memory
+# name = 5 # error: can't assign int to str
+
+age: int = 28  # Integer Data Type (Numbers without decimal points)
+print(type(age))
+print(age)
+
+pi: float = 3.14  # Float Data Type (Numbers with decimal points)
+print(type(pi))
+print(pi)
+
+bool_variable: bool = True  # Boolean Data Type
+print(type(bool_variable))
+print(bool_variable)
