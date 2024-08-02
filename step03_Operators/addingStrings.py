7a79cb244d670920f72c2b56ce83ab818f4aff50
saix511 <saadirfan41@gmail.com>
2024-08-02 20:50:38 +0500

    added step 3 and step 4
    Operators and List DataType

---
+++ step03_Operators/addingStrings.py
@@ -0,0 +1,18 @@
+#
+# ?  How to ADD a string and a variable in a single print statement using + operator
+name = "Saad Irfan"
+print("Hi my name is" + " " + name)
+
+# ? Another way is just using a comma ','
+print("Hi my name is ", name)
+
+# ? Another way is using f string
+print(f"Hi my name is {name}")
+
+# ? Adding number with string
+age = 21
+print("My age is " + str(age))
+# ? Or by using comma
+print("My age is ", age)
+# ? Or by using f string
+print(f"My age is {age:.2F}")
